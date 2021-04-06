d5ca5547e7beddbaa0fa3295b88a94fc5b2e2e1f
CarlosGutierrezArdila <carlos.gutierrez@mercadolibre.com.co>
2021-04-06 09:36:45 -0500

    create challenge project

---
+++ gutierrez_carlos/src/main/java/com/gutierrez_carlos/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.gutierrez_carlos.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+
+}
---
+++ gutierrez_carlos/src/test/java/com/gutierrez_carlos/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutierrez_carlos.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
